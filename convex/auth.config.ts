8a980359c18f547c5bb092a6a4639e5c04087c94
Sandip-Chavda <chavdas766@gmail.com>
2024-06-24 12:08:53 +0530

    Clerk (Authentication) & Convex (DataBase) Setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://enabling-gnu-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ middleware.ts
@@ -0,0 +1,19 @@
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({});
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/(.*)", "/"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
