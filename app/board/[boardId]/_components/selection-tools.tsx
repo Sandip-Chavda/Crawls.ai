44e337543a4992c8d83277c4e105d5296167b569
Sandip-Chavda <chavdas766@gmail.com>
2024-07-04 16:36:38 +0530

    Bring layer to front and send to back added

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -8,6 +8,8 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import Hint from "@/components/hint";
 import { Button } from "@/components/ui/button";
+import { RiBringToFront } from "react-icons/ri";
+import { RiSendToBack } from "react-icons/ri";
 import { Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
@@ -18,6 +20,51 @@ interface SelectionToolsProps {
 const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
+
+    const moveToBack = useMutation(
+      ({ storage }) => {
+        const liveLayersIds = storage.get("layerIds");
+
+        const indices: number[] = [];
+
+        const arr = liveLayersIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+          liveLayersIds.move(indices[i], i);
+        }
+      },
+      [selection]
+    );
+
+    const moveToFront = useMutation(
+      ({ storage }) => {
+        const liveLayersIds = storage.get("layerIds");
+
+        const indices: number[] = [];
+
+        const arr = liveLayersIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+          liveLayersIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          );
+        }
+      },
+      [selection]
+    );
 
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
@@ -50,7 +97,35 @@ const SelectionTools = memo(
         )`,
         }}
       >
+        {/* Color Picker pallete */}
         <ColorPicker onChange={setFill} />
+
+        {/* Layer Bring to Front & Send to Back  */}
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label="Bring to front">
+            <Button
+              size="icon"
+              variant="board"
+              className=""
+              onClick={moveToFront}
+            >
+              <RiBringToFront size={24} />
+            </Button>
+          </Hint>
+
+          <Hint label="Send to back">
+            <Button
+              size="icon"
+              variant="board"
+              className=""
+              onClick={moveToBack}
+            >
+              <RiSendToBack size={24} />
+            </Button>
+          </Hint>
+        </div>
+
+        {/* Delete Button */}
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
           <Hint label="Delete Layer">
             <Button
